4abb00b1fe5885ee859218c60c16e84d18aae8e5
代码搬运工 <44076975+xuxuinfighting@users.noreply.github.com>
2019-08-05 18:20:08 +0800

    Update 数据链表leetcode.py

--- 第一次作业/数据链表leetcode.py
+++ 第一次作业/数据链表leetcode.py
@@ -22,3 +22,44 @@ class Solution(object):
                 else:
                     k = k - 1
         return res
+    
+"""leetcode 第169题 找众数"""
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        num_dict = {}
+        for i in nums:
+        	if i in num_dict:
+        		num_dict[i] += 1
+        	else :
+        		num_dict[i] = 1
+        return max(num_dict.items(),key = lambda x : x[1])[0]
+
+"""leetcode 第41题 寻找第一个丢失的正整数"""
+TODO
+
+
+"""leetcode 第141题 是否存在环形"""
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+ 
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        if not head:
+            return False
+        p1=p2=head#p1每次跑1步，p2每次跑2步
+        while p2.next and p2.next.next:#判断跑得快的是否为空
+            p1=p1.next
+            p2=p2.next.next
+            if p1==p2:#存在环则必然会出现相等
+                return True
+        return False
